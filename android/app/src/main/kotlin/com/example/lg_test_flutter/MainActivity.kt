260d9fa858ad2ecd012d11500d832f5e0dd5d95b
Maiorov Alexander <maiorov.work@gmail.com>
2023-07-24 09:26:05 +0300

    Init

---
+++ android/app/src/main/kotlin/com/example/lg_test_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lg_test_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/repositories/news/abstract_news_repository.dart
@@ -0,0 +1,7 @@
+import 'package:lg_test_flutter/repositories/news/repository.dart';
+
+abstract class AbstractNewsRepository {
+  Future<List<Article>> getLatestArticles();
+  Future<List<Article>> getFeaturedArticles();
+  Future<Article> getArticle(String id);
+}
